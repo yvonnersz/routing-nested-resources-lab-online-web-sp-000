01a11e8b16003119611865794b0b86df908fc013
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-24 07:50:48 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
-      artist = Artist.find_by(:id => params[:artist_id])
+      @artist = Artist.find_by(:id => params[:artist_id])
       if artist.nil?
         flash[:alert] = "Song not found."
         redirect_to artists_path
