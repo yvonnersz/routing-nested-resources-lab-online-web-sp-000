7078b0e47c1e49745d73c82def3daa0f0f5ad404
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-24 07:44:43 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Song.find(params[:artist_id])
+      @songs = Song.find(params[:artist_id]).songs
     else
       @songs = Song.all
     end
